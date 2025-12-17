e187895fe16cea063690ec529b0aac8e97a817c3
ranichelli <chellirani8@gmail.com>
2025-12-17 13:19:04 +0530

    Add files via upload

---
+++ tests-jasmine/utils/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('work with zero', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds upto nearest cent', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+});
